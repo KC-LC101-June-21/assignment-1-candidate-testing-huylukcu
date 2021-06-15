85eda593f01e68d78aee9b34d56a991444580ac7
huylukcu <>
2021-06-15 20:01:51 +0000

    Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,15 +2,19 @@ const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
+let questions = ["1) Who was the first American woman in space? ", "2) True or false: 5 kilometer == 5000 meters? ", "3) (5 + 3)/2 * 10 = ? ", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "5) What is the minimum crew size for the ISS? "];
+
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+//let questions;
+//let correctAnswers;
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -22,7 +26,16 @@ function askForName() {
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-  candidateAnswer = input.question("Who was the first American woman in space? ");
+    for(let i = 0; i < questions.length; i++){
+      candidateAnswers[i] = input.question(questions[i]) ;
+        console.log(`Your Answer: ${candidateAnswers[i]}`);
+        console.log(`Correct Answer: ${correctAnswers[i]}\n`);
+      if(candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
+      console.log("Your answer is right!\n")
+      }else{
+      console.log("Incorrect answer!\n")
+      }
+  }
 
 
 }
@@ -30,7 +43,7 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if(candidateAnswer===correctAnswer.toLowerCase()){
+  if(candidateAnswer.toLocaleLowerCase()===correctAnswer.toLowerCase()){
     console.log("Correct!");
 
   }else{
